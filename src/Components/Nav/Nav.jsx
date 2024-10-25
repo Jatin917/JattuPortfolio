7990dc89711c17123f5d3439ca3de0f2454ee4b6
Jatin917 <jkchandel02@gmail.com>
2024-10-25 10:41:02 +0530

    solve path issue

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Nav.scss'
 import {motion} from 'framer-motion'
-import gfg from '../../../public/gfg.svg'
-import Leetcode from '../../../public/Leetcode.png'
-import twitter from '../../../public/twitter.png'
-import linkedin from '../../../public/linkedin.png'
-import github from '../../../public/github.png'
+import gfg from '/gfg.svg'
+import Leetcode from '/Leetcode.png'
+import twitter from '/twitter.png'
+import linkedin from '/linkedin.png'
+import github from '/github.png'
 import logo from '../../resources/j-logo-zip-file/png/logo-color.png'
 import Sidebar from '../sidebar/Sidebar'
 const variants = {
@@ -48,7 +48,7 @@ const Nav = () => {
                 <motion.span variants={variants} >JATIN CHANDEL 😎</motion.span>
             </motion.div>
             <motion.div variants={logoVariants}  initial="initial" animate="animate" className="social">
-                <motion.a href="https://leetcode.com/u/Jatin917/" target='_blank'><motion.img variants={logoVariants} src={Leetcode}/></motion.a>
+                <motion.a href="https://leetcode.com/u/Jatin917/" target='_blank'><motion.img variants={logoVariants} src="/public/LeetCode.png" /></motion.a>
                 <motion.a href="https://www.geeksforgeeks.org/user/jkchanoqtv/" target='_blank'><motion.img variants={logoVariants}  src={gfg}/></motion.a>
                 <motion.a href="https://www.linkedin.com/in/jatin-chandel-703a7b1ab?originalSubdomain=in" target='_blank'><motion.img variants={logoVariants}  src={linkedin}/></motion.a>
                 <motion.a href="https://x.com/JatinChandel13?s=09" target='_blank'><motion.img variants={logoVariants}  src={twitter}/></motion.a>
