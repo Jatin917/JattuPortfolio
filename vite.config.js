57a8de489b39076034d4f4c384e86b69c8fbee64
Jatin917 <jkchandel02@gmail.com>
2024-10-31 20:56:10 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   cacheDir: '.vite_cache',
+  build: {
+    rollupOptions: {
+      external: ['/assets/FlimpireLg.png']
+    }
+  }
 })
 
